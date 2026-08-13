11263d391b48f8cef7f45a0070d99ebc7b09f1ea
shrinainakoppula-alt <shrinainakoppula@gmail.com>
2026-08-13 14:52:27 +0530

    Initial Commit

---
+++ src/main/java/SEGroup/MavenJavaProject/App.java
@@ -0,0 +1,10 @@
+package SEGroup.MavenJavaProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    	System.out.println("This is the first Java Project in Eclipse");
+    }
+}
